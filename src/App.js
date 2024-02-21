b6ad9058b9a9f3e8bee56bb68ec1a10da6fb0476
Dhruvpathak25 <dapathak.dp@gmail.com>
2024-02-21 17:56:30 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import CountryApp from './CountryApp.js'; 
 
-function App() {
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  
+  </div>
+);
+
+
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [sortType, setSortType] = useState('name');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [countriesPerPage, setCountriesPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://restcountries.com/v2/all');
+        const data = await response.json();
+        setCountries(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    setSelectedCountry(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <header className="app-header">
+          <h1>React App</h1>
+        </header>
+
+        <nav className="navbar">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/country-app">Country App</Link>
+            </li>
+            <li>
+              <Link to="/material-ui-app">Material UI App</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/country-app" element={<CountryApp />} />
+        
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
